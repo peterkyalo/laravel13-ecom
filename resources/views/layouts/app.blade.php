8dc020bd4f573807a5edd501c47584dc01dc1978
Peter Nzingu <peterkyalo69@gmail.com>
2026-04-04 19:48:38 +0300

    Add new layout components for header, footer, and slider
    - Implemented a responsive header with navigation and search functionality.
    - Created a footer with company information, categories, and social media links.
    - Added a main slider section showcasing featured products with Swiper integration.
    - Included necessary scripts and styles for Tailwind CSS and Font Awesome.
    - Established mobile menu functionality for better user experience.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,20 +1,44 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
+    <?php
+    include 'includes/head.php';
+    ?>
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="font-sans text-gray-600 antialiased">
+
+    <?php
+    include 'includes/header.php';
+    ?>
+
+
+    <!-- Main Content Start -->
+
+    {{ $slot }}
+
+    <!-- Main Content End -->
+
+    <?php
+    include 'includes/footer.php';
+    ?>
+
+    <?php
+    include 'includes/script.php';
+    ?>
+
+</body>
+{{-- <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
             @include('layouts.navigation')
 
@@ -32,5 +56,6 @@
                 {{ $slot }}
             </main>
         </div>
-    </body>
+    </body> --}}
+
 </html>
